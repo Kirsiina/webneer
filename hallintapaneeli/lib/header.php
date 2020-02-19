90113659447348ff8edcd84f1d2c6fde46f6c1eb
Pndy <Pndy@users.noreply.github.com>
2020-02-19 08:33:10 +0200

    hallintapaneelin alku

--- hallintapaneeli/lib/header.php
+++ hallintapaneeli/lib/header.php
@@ -3,3 +3,26 @@
 include '../lib/config.php';
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <title>Webneer - Hallintapaneeli</title>
+
+    <meta charset="utf-8">
+
+    <link href="https://fonts.googleapis.com/css?family=Bebas+Neue|Open+Sans&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="author" content="Kirsi Suoranta, Jenna Aaltonen, Petri Paukkunen, Roope Pennanen">
+
+    <meta name="description" content="All kind of web services for all.">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="https://kit.fontawesome.com/5f0207882f.js"></script>
+
+  </head>
