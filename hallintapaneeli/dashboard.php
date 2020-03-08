9d36954a11993a289b9ed5fcc05b8d684670a0e4
Pndy <Pndy@users.noreply.github.com>
2020-03-08 21:05:31 +0200

    dashboard orders panel

--- hallintapaneeli/dashboard.php
+++ hallintapaneeli/dashboard.php
@@ -10,6 +10,10 @@ $regamount = mysqli_fetch_row($result);
 $query = "SELECT COUNT(*) FROM `webneer_yhteydenottolomake` WHERE pvm BETWEEN (CURRENT_TIMESTAMP - INTERVAL 30 DAY) AND CURRENT_TIMESTAMP";
 $result = mysqli_query($yhteys, $query);
 $lomakeamount = mysqli_fetch_row($result);
+
+$query = "SELECT COUNT(*) FROM `webneer_tilaukset` WHERE tilauspvm > (NOW() - INTERVAL 30 DAY) ";
+$result = mysqli_query($yhteys, $query);
+$tilausamount = mysqli_fetch_row($result);
 ?>
 
 <body>
@@ -32,6 +36,13 @@ $lomakeamount = mysqli_fetch_row($result);
             <div class="card text-center">
                 <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Feedback (Past 30 days)</div>
                 <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $lomakeamount[0]; ?></div>
+            </div>
+        </div>
+
+        <div class="col-xl-3 col-md-6 mb-4">
+            <div class="card text-center">
+                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Orders (Past 30 days)</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $tilausamount[0]; ?></div>
             </div>
         </div>
 
